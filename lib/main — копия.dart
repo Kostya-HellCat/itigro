6ca5fcd6975424062370d93ceec1afcd263386b4
Kostya-HellCat <kostik.atroshenkov@mail.ru>
2019-09-26 19:06:50 +0300

    test generator

---
+++ lib/main — копия.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  _getJSON(i) async{
+    var response = await http.get('https://randomuser.me/api?results=10'); //https://randomuser.me/api or https://api.github.com/users
+    if (response.statusCode == 200){
+      var _jsonMap = json.decode(response.body);
+      if (_jsonMap['results'][i] != null) {
+          setState(() {
+            print(_jsonMap['results'][i]['name']['first']);
+          });
+      }
+    }
+    else{
+      print('Status[$i] =  ${response.statusCode} ${response.body}');
+    }
+  }
+
+  _createItem(usersArray){
+    return new GestureDetector(
+      onTap: () {},
+      child: Container(
+          decoration: const BoxDecoration(
+            border: Border(
+              bottom: BorderSide(width: 1.0, color: Color(0xFFEEEEEE)),
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    flex: 3,
+                    child: Padding(
+                        padding: EdgeInsets.only(right: 12.0),
+                        child: Container(
+                          decoration: new BoxDecoration(
+                              image: new DecorationImage(
+                                fit: BoxFit.cover,
+                                alignment: FractionalOffset.center,
+                                image: new NetworkImage('http://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9'),
+                              )
+                          ),
+                          width: 75,
+                          height: 75,
+                        )
+                    )
+                ),
+
+                Expanded(
+                  flex: 10,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        usersArray.toString(),
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Flutter\'s hot reload helps you quickly and easily experiment, build UIs, add features, and fix bug faster. Experience sub-second reload times, without losing state, on emulators, simulators, and hardware for iOS and Android.',
+                        style: TextStyle(color: Color(0xFF757575)),),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+      ),
+    );
+  }
+
+  List<String> usersArray;
+  var wid;
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          if (index < 5) {
+            _getJSON(index).then(wid = _createItem(usersArray));
+            return wid;
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+        itemCount: 5 + 1,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,121 +3,72 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyBody extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: ''),
-    );
-  }
+
+  createState() => new MyBodyState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyBodyState extends State<MyBody> {
+  var controller = new StreamController<List<String>>.broadcast();
+  List<String> _array = [''];
+  int j = 0;
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  _getUser(i) async{
-    var response = await http.get('https://randomuser.me/api?results=10'); //https://randomuser.me/api or https://api.github.com/users
-    if (response.statusCode == 200){
-      var _jsonMap = json.decode(response.body);
-      if (_jsonMap['results'][i] != null) {
-        setState(() {
-          return _createItem(_jsonMap['results'][i]['name']['first']);
-        });
-      }
-
-    }
-    else{
-      print('Status[$i] =  ${response.statusCode} ${response.body}');
+  arrGenerate() async {
+    int i = 0;
+    while (i != 5) {
+      await Future.delayed(const Duration(seconds: 1));
+      _array.add('$i');
+      controller.add(_array);
+      print('Controller add ${i*2} item');
+      i++;
     }
   }
 
-  Widget _createItem(userLogin){
-    return new GestureDetector(
-      onTap: () {},
-      child: Container(
-          decoration: const BoxDecoration(
-            border: Border(
-              bottom: BorderSide(width: 1.0, color: Color(0xFFEEEEEE)),
-            ),
-          ),
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                    flex: 3,
-                    child: Padding(
-                        padding: EdgeInsets.only(right: 12.0),
-                        child: Container(
-                          decoration: new BoxDecoration(
-                              image: new DecorationImage(
-                                fit: BoxFit.cover,
-                                alignment: FractionalOffset.center,
-                                image: new NetworkImage('http://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9'),
-                              )
-                          ),
-                          width: 75,
-                          height: 75,
-                        )
-                    )
-                ),
-
-                Expanded(
-                  flex: 10,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        userLogin.toString(),
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        'Flutter\'s hot reload helps you quickly and easily experiment, build UIs, add features, and fix bug faster. Experience sub-second reload times, without losing state, on emulators, simulators, and hardware for iOS and Android.',
-                        style: TextStyle(color: Color(0xFF757575)),),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )
-      ),
-    );
-  }
-
-  var userLogin;
+//   getJson(i) async{
+//    var response = await http.get('https://randomuser.me/api?results=10'); //https://randomuser.me/api or https://api.github.com/users
+//    if (response.statusCode == 200){
+//      var _jsonMap = json.decode(response.body);
+//      print('${_jsonMap['results'][i]['name']['first']}');
+//      if ((_jsonMap['results'].length - 1 >= i)) {
+//        _nameArray.addAll(['${_jsonMap['results'][i]['name']['first']}']);
+//      }
+//    }
+//    else{
+//      print('Error. Status =  ${response.statusCode} ${response.body}');
+//    }
+//  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          if (index < 5) {
-            return _getUser(index);
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-        itemCount: 5 + 1,
-      ),
+    arrGenerate();
+
+    return StreamBuilder<List<String>>(
+        stream: controller.stream,
+        builder: (context, snapshot) {
+          return ListView.builder(itemBuilder: (context, index) {
+            if (index.isOdd) return new Divider();
+            index = index ~/ 2;
+            if (_array[index] != null) {
+              print('$index, будет выведено ${_array[index]}');
+
+              return new Text('${_array[index]}');
+            }
+            else{
+              return new Text('');
+            }
+
+          });
+        }
     );
   }
+
+
 }
+
+void main() =>  runApp(
+    new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: new Scaffold(body: new Center(child: new MyBody(),))
+    )
+);
